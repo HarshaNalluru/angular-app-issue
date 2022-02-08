c4425f0eb8d6c2fec09d8470a41299d38eab871d
HarshaNalluru <sanallur@microsoft.com>
2022-02-07 17:07:27 -0800

    listSettings test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { AppConfigurationClient } from '@azure/app-configuration';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-app-issue';
 }
+
+async function listSettings() {
+  const connectionString = '<connection-string>';
+  const client = new AppConfigurationClient(connectionString);
+  const sampleKeys = client.listConfigurationSettings({});
+  for await (const setting of sampleKeys) {
+    console.log(`  Found key: ${setting.key}, label: ${setting.label}`);
+  }
+}
+
+listSettings().catch((err) => {
+  console.log(err);
+});
